d4cec24cb70fa7854f46d2e96ab98867247d8015
Geoffroy Oudoumanessah <geoffroy.oudoumanessah@grenoble-inp.org>
2020-05-02 19:28:36 +0200

    ajout script python + calcul erreur

---
+++ script/lambda.py
@@ -0,0 +1,20 @@
+"""
+    Script to plot the effect
+    of lambda on the Schwarz without
+    overlapping method
+"""
+import matplotlib.pyplot as plt
+
+xx = open("../output/data/lambdax.txt").read().split()
+xx = [0] + [float(elt) for elt in xx]
+
+yy = open("../output/data/lambday.txt").read().split()
+yy = [float(elt) for elt in yy]
+
+
+plt.plot(xx,yy,)
+plt.xlabel("lambda")
+plt.ylabel("error")
+plt.title("Effect of lambda on the error")
+plt.show()
+plt.savefig("effectlambda.png")
---
+++ script/plot.sh
@@ -0,0 +1,3 @@
+freefem++ ../src/lambda.edp
+python3 lambda.py
+mv effectlambda.png ../output/graphics
